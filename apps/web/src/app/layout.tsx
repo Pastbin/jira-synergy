139b71f298d4cf9cab9039e4d4d4f5e3dd3e143e
pastbin <pastbin.alex@gmail.com>
2026-02-16 15:58:58 +0400

    feat(web): изменен язык интерфейса на русский и добавлено подавление предупреждений гидратации в корневом layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="ru" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
